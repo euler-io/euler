e9b689592cfc9800a1501808fdb2cf1dffdd4141
filipecsimoes <835586+filipesimoes@users.noreply.github.com>
2020-02-17 18:44:02 -0300

    processing context

--- euler-core/src/main/java/com/github/euler/core/ProcessingContext.java
+++ euler-core/src/main/java/com/github/euler/core/ProcessingContext.java
@@ -1,40 +1,119 @@
 package com.github.euler.core;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ProcessingContext {
 
-    private Map<String, Object> metadata;
+    private final Map<String, Object> metadata;
+    private final Map<String, Object> context;
+    private final Action action;
 
-    public ProcessingContext(Map<String, Object> metadata) {
+    private ProcessingContext(Map<String, Object> metadata, Map<String, Object> context, Action action) {
         this.metadata = Collections.unmodifiableMap(new HashMap<>(metadata));
+        this.context = Collections.unmodifiableMap(new HashMap<>(context));
+        this.action = action;
     }
 
     public Object metadata(String key) {
         return this.metadata.get(key);
     }
 
+    public Map<String, Object> metadata() {
+        return metadata;
+    }
+
+    public Object context(String key) {
+        return this.context.get(key);
+    }
+
+    public Map<String, Object> context() {
+        return context;
+    }
+
     public static Builder builder() {
         return new Builder();
+    }
+
+    public ProcessingContext merge(ProcessingContext o) {
+        Map<String, Object> newMetadata = merge(this.metadata, o.metadata, this.action);
+        Map<String, Object> newContext = new HashMap<>(this.context);
+        return new ProcessingContext(newMetadata, newContext, this.action);
+    }
+
+    private Map<String, Object> merge(Map<String, Object> m1, Map<String, Object> m2, Action action) {
+        Map<String, Object> merged = new HashMap<>(m1);
+
+        switch (action) {
+            case OVERWRITE :
+                merged.putAll(m2);
+                break;
+            case MERGE :
+                m2.forEach((k, v) -> putIfAbsentOrMerge(merged, k, v));
+                break;
+            default :
+                m2.forEach(merged::putIfAbsent);
+                break;
+        }
+
+        return merged;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private void putIfAbsentOrMerge(Map<String, Object> payload, String key, Object value) {
+        Object current = payload.get(key);
+        if (current == null) {
+            payload.put(key, value);
+        } else if (current instanceof List && value instanceof List) {
+            List newList = new ArrayList((List) current);
+            newList.addAll((List) value);
+            payload.put(key, newList);
+        } else if (current instanceof Set && value instanceof Set) {
+            Set newSet = new HashSet((Set) current);
+            newSet.addAll((Set) value);
+            payload.put(key, newSet);
+        }
+    }
+
+    public static enum Action {
+        PUT_IF_ABSENT, OVERWRITE, MERGE
     }
 
     public static class Builder {
 
         private Map<String, Object> metadata;
+        private Map<String, Object> context;
+        private Action action = Action.PUT_IF_ABSENT;
 
         public Builder() {
             super();
             this.metadata = new HashMap<>();
+            this.context = new HashMap<>();
         }
 
-        public void metadata(String key, String value) {
+        public void metadata(String key, Object value) {
             this.metadata.put(key, value);
         }
 
         public ProcessingContext build() {
-            return new ProcessingContext(this.metadata);
+            return new ProcessingContext(this.metadata, this.context, this.action);
+        }
+
+        public void context(String key, Object value) {
+            this.context.put(key, value);
+        }
+
+        public Action getAction() {
+            return action;
+        }
+
+        public void setAction(Action action) {
+            this.action = action;
         }
 
     }
--- euler-core/src/test/java/com/github/euler/core/ProcessingContextTest.java
+++ euler-core/src/test/java/com/github/euler/core/ProcessingContextTest.java
@@ -1,13 +1,19 @@
 package com.github.euler.core;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 
 public class ProcessingContextTest {
 
     @Test
-    public void testBuildContextWithMetadata() {
+    public void testBuildProcessingContextWithMetadata() {
         ProcessingContext.Builder builder = ProcessingContext.builder();
 
         String key = "key";
@@ -19,7 +25,7 @@ public class ProcessingContextTest {
     }
 
     @Test
-    public void testBuildContextWithMultipleMetadata() {
+    public void testBuildProcessingContextWithMultipleMetadata() {
         ProcessingContext.Builder builder = ProcessingContext.builder();
 
         String key1 = "key1";
@@ -32,7 +38,181 @@ public class ProcessingContextTest {
 
         ProcessingContext ctx = builder.build();
         assertEquals(value1, ctx.metadata(key1));
-        assertEquals(value2, ctx.metadata(key2));
+        assertEquals(value1, ctx.metadata().get(key1));
+        assertEquals(2, ctx.metadata().size());
 
+        assertEquals(value2, ctx.metadata(key2));
+    }
+
+    @Test
+    public void testBuildProcessingContextWithContext() {
+        ProcessingContext.Builder builder = ProcessingContext.builder();
+
+        String key = "key";
+        String value = "value";
+        builder.context(key, value);
+
+        ProcessingContext ctx = builder.build();
+        assertEquals(value, ctx.context(key));
+
+    }
+
+    @Test
+    public void testBuildProcessingContextWithMultiplesContext() {
+        ProcessingContext.Builder builder = ProcessingContext.builder();
+
+        String key1 = "key1";
+        String value1 = "value1";
+        builder.context(key1, value1);
+
+        String key2 = "key2";
+        String value2 = "value2";
+        builder.context(key2, value2);
+
+        ProcessingContext ctx = builder.build();
+        assertEquals(value1, ctx.context(key1));
+        assertEquals(value1, ctx.context().get(key1));
+        assertEquals(2, ctx.context().size());
+
+        assertEquals(value2, ctx.context(key2));
+
+    }
+
+    @Test
+    public void testMergePutIfAbsentMetadata() {
+        ProcessingContext.Builder builder1 = ProcessingContext.builder();
+        String key1 = "key1";
+        String value1 = "value1";
+        builder1.metadata(key1, value1);
+        builder1.setAction(ProcessingContext.Action.PUT_IF_ABSENT);
+
+        ProcessingContext ctx1 = builder1.build();
+
+        ProcessingContext.Builder builder2 = ProcessingContext.builder();
+        String key2 = "key2";
+        String value2 = "value2";
+        builder2.metadata(key2, value2);
+        builder2.setAction(ProcessingContext.Action.PUT_IF_ABSENT);
+
+        ProcessingContext ctx2 = builder2.build();
+
+        ProcessingContext ctx = ctx1.merge(ctx2);
+
+        assertEquals(value1, ctx.metadata(key1));
+        assertEquals(value2, ctx.metadata(key2));
+    }
+
+    @Test
+    public void testMergePutIfAbsentMetadataWontReplace() {
+        ProcessingContext.Builder builder1 = ProcessingContext.builder();
+        String key1 = "key1";
+        String value1 = "value1";
+        builder1.metadata(key1, value1);
+        builder1.setAction(ProcessingContext.Action.PUT_IF_ABSENT);
+
+        ProcessingContext ctx1 = builder1.build();
+
+        ProcessingContext.Builder builder2 = ProcessingContext.builder();
+        String value2 = "value2";
+        builder2.metadata(key1, value2);
+        builder2.setAction(ProcessingContext.Action.PUT_IF_ABSENT);
+
+        ProcessingContext ctx2 = builder2.build();
+
+        ProcessingContext ctx = ctx1.merge(ctx2);
+
+        assertEquals(value1, ctx.metadata(key1));
+    }
+
+    @Test
+    public void testMergeOverwriteMetadata() {
+        ProcessingContext.Builder builder1 = ProcessingContext.builder();
+        String key1 = "key1";
+        String value1 = "value1";
+        builder1.metadata(key1, value1);
+        builder1.setAction(ProcessingContext.Action.PUT_IF_ABSENT);
+
+        ProcessingContext ctx1 = builder1.build();
+
+        ProcessingContext.Builder builder2 = ProcessingContext.builder();
+        String value2 = "value2";
+        builder2.metadata(key1, value2);
+        builder2.setAction(ProcessingContext.Action.OVERWRITE);
+
+        ProcessingContext ctx2 = builder2.build();
+
+        ProcessingContext ctx = ctx1.merge(ctx2);
+
+        assertEquals(value2, ctx.metadata(key1));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testMergeListMetadata() {
+        ProcessingContext.Builder builder1 = ProcessingContext.builder();
+        String key1 = "key1";
+        String value1 = "value1";
+        builder1.metadata(key1, value1);
+
+        String collectionKey = "col1";
+        builder1.metadata(collectionKey, Arrays.asList("a"));
+        builder1.setAction(ProcessingContext.Action.PUT_IF_ABSENT);
+
+        ProcessingContext ctx1 = builder1.build();
+
+        ProcessingContext.Builder builder2 = ProcessingContext.builder();
+        String key2 = "key2";
+        String value2 = "value2";
+        builder2.metadata(key2, value2);
+        builder2.metadata(collectionKey, Arrays.asList("b"));
+        builder2.setAction(ProcessingContext.Action.MERGE);
+
+        ProcessingContext ctx2 = builder2.build();
+
+        ProcessingContext ctx = ctx1.merge(ctx2);
+
+        assertEquals(value1, ctx.metadata(key1));
+        assertEquals(value2, ctx.metadata(key2));
+        assertEquals(3, ctx.metadata().size());
+
+        List<String> list = (List<String>) ctx.metadata(collectionKey);
+        assertEquals(2, list.size());
+        assertEquals("a", list.get(0));
+        assertEquals("b", list.get(1));
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Test
+    public void testMergeSetMetadata() {
+        ProcessingContext.Builder builder1 = ProcessingContext.builder();
+        String key1 = "key1";
+        String value1 = "value1";
+        builder1.metadata(key1, value1);
+
+        String collectionKey = "col1";
+        builder1.metadata(collectionKey, new HashSet(Arrays.asList("a")));
+        builder1.setAction(ProcessingContext.Action.PUT_IF_ABSENT);
+
+        ProcessingContext ctx1 = builder1.build();
+
+        ProcessingContext.Builder builder2 = ProcessingContext.builder();
+        String key2 = "key2";
+        String value2 = "value2";
+        builder2.metadata(key2, value2);
+        builder2.metadata(collectionKey, new HashSet(Arrays.asList("b")));
+        builder2.setAction(ProcessingContext.Action.MERGE);
+
+        ProcessingContext ctx2 = builder2.build();
+
+        ProcessingContext ctx = ctx1.merge(ctx2);
+
+        assertEquals(value1, ctx.metadata(key1));
+        assertEquals(value2, ctx.metadata(key2));
+        assertEquals(3, ctx.metadata().size());
+
+        Set<String> set = (Set<String>) ctx.metadata(collectionKey);
+        assertEquals(2, set.size());
+        assertTrue(set.contains("a"));
+        assertTrue(set.contains("b"));
     }
 }
