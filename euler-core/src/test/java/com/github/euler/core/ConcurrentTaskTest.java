ad5e4b4647ba04ae97eb6f4f32f6d9d3f8d9c03d
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2020-02-19 22:33:44 -0300

    Processing context

--- euler-core/src/test/java/com/github/euler/core/ConcurrentTaskTest.java
+++ euler-core/src/test/java/com/github/euler/core/ConcurrentTaskTest.java
@@ -2,6 +2,7 @@ package com.github.euler.core;
 
 import java.net.URI;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.github.euler.AkkaTest;
@@ -54,7 +55,6 @@ public class ConcurrentTaskTest extends AkkaTest {
     @Test
     public void testWhenTaskFailReplyToProcessorWithJobTaskFailed() throws Exception {
         Task task = Tasks.accept("task", () -> WillFailBehavior.create());
-
         Task concurrentTask = new ConcurrentTask("concurrent-task", task);
 
         TestProbe<ProcessorCommand> probe = testKit.createTestProbe();
@@ -64,5 +64,22 @@ public class ConcurrentTaskTest extends AkkaTest {
         ref.tell(msg);
 
         probe.expectMessageClass(JobTaskFailed.class);
+    }
+
+    @Test
+    @Ignore
+    public void testWhenJobTaskAndTaskFinishedReplyJobTaskFinished() throws Exception {
+        Task task1 = Tasks.empty("task-1");
+        Task task2 = Tasks.empty("task-2");
+        Task concurrentTask = new ConcurrentTask("concurrent-task", task1, task2);
+
+        TestProbe<ProcessorCommand> probe = testKit.createTestProbe();
+        ActorRef<TaskCommand> ref = testKit.spawn(concurrentTask.behavior());
+
+        JobTaskToProcess msg = new JobTaskToProcess(new URI("file:///some/path"), new URI("file:///some/path/item"), ProcessingContext.EMPTY, probe.ref());
+        ref.tell(msg);
+
+        probe.expectMessageClass(JobTaskFinished.class);
+        probe.expectNoMessage();
     }
 }
