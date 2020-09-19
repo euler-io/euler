65164fd488ba32421445c5f76709883514238775
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2020-09-18 22:24:32 -0300

    #16

--- euler-file/src/main/java/com/github/euler/file/FileStorageStrategyConfigConverter.java
+++ euler-file/src/main/java/com/github/euler/file/FileStorageStrategyConfigConverter.java
@@ -1,6 +1,8 @@
 package com.github.euler.file;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import com.github.euler.common.AbstractStorageStrategyConfigConverter;
 import com.github.euler.common.StorageStrategy;
@@ -10,11 +12,26 @@ import com.typesafe.config.Config;
 
 public class FileStorageStrategyConfigConverter extends AbstractStorageStrategyConfigConverter {
 
+    public static final String TEMPORARY_ROOT_FOLDER = "temporary-folder";
+
     @Override
     public StorageStrategy convert(Config config, ConfigContext configContext, TypesConfigConverter typeConfigConverter) {
-        File root = null;
-        String suffix = null;
+        File root = getRoot(config);
+        String suffix = config.getString("suffix");
         return new FileStorageStrategy(root, suffix);
+    }
+
+    private File getRoot(Config config) {
+        String root = config.getString("root");
+        if (root.equals(TEMPORARY_ROOT_FOLDER)) {
+            try {
+                return Files.createTempDirectory("euler").toFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return new File(root);
+        }
     }
 
     @Override
--- euler-tika/src/main/java/com/github/euler/tika/TikaExtension.java
+++ euler-tika/src/main/java/com/github/euler/tika/TikaExtension.java
@@ -6,18 +6,12 @@ import java.util.List;
 import com.github.euler.configuration.ContextConfigConverter;
 import com.github.euler.configuration.EulerExtension;
 import com.github.euler.configuration.TaskConfigConverter;
-import com.github.euler.configuration.TypeConfigConverter;
 
 public class TikaExtension implements EulerExtension {
 
     @Override
     public List<ContextConfigConverter> pathConverters() {
         return Arrays.asList(new TikaContextConfigConverter());
-    }
-
-    @Override
-    public List<TypeConfigConverter<?>> typeConverters() {
-        return EulerExtension.super.typeConverters();
     }
 
     @Override
