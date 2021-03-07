c49e2456a62da2fe09ebac3eab1ca85ad9e7be64
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2021-03-07 17:14:15 -0300

    #20

---
+++ euler-core/src/main/java/com/github/euler/core/Initializable.java
@@ -0,0 +1,9 @@
+package com.github.euler.core;
+
+import java.io.IOException;
+
+public interface Initializable {
+
+    void initialize() throws IOException;
+
+}
--- euler-core/src/main/java/com/github/euler/core/JobExecution.java
+++ euler-core/src/main/java/com/github/euler/core/JobExecution.java
@@ -1,5 +1,7 @@
 package com.github.euler.core;
 
+import java.io.IOException;
+
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.javadsl.AbstractBehavior;
@@ -14,15 +16,25 @@ public class JobExecution extends AbstractBehavior<JobCommand> {
         return Behaviors.setup((context) -> new JobExecution(context, sourceBehavior, processorBehavior));
     }
 
+    public static Behavior<JobCommand> create(Behavior<SourceCommand> sourceBehavior, Behavior<ProcessorCommand> processorBehavior, EulerHooks hooks) {
+        return Behaviors.setup((context) -> new JobExecution(context, sourceBehavior, processorBehavior, hooks));
+    }
+
     private final Behavior<SourceCommand> sourceBehavior;
     private final Behavior<ProcessorCommand> processorBehavior;
+    private final EulerHooks hooks;
 
     private ActorRef<EulerCommand> eulerRef;
 
-    private JobExecution(ActorContext<JobCommand> context, Behavior<SourceCommand> sourceBehavior, Behavior<ProcessorCommand> processorBehavior) {
+    private JobExecution(ActorContext<JobCommand> context, Behavior<SourceCommand> sourceBehavior, Behavior<ProcessorCommand> processorBehavior, EulerHooks hooks) {
         super(context);
         this.sourceBehavior = sourceBehavior;
         this.processorBehavior = processorBehavior;
+        this.hooks = hooks;
+    }
+
+    private JobExecution(ActorContext<JobCommand> context, Behavior<SourceCommand> sourceBehavior, Behavior<ProcessorCommand> processorBehavior) {
+        this(context, sourceBehavior, processorBehavior, new EulerHooks());
     }
 
     @Override
@@ -35,17 +47,31 @@ public class JobExecution extends AbstractBehavior<JobCommand> {
     }
 
     private Behavior<JobCommand> onJob(Job msg) {
-        this.eulerRef = getContext().spawn(EulerJobProcessor.create(sourceBehavior, processorBehavior), "euler");
-        eulerRef.tell(new JobToProcess(msg.uri, msg.ctx, msg.replyTo));
-        return Behaviors.same();
+        try {
+            this.hooks.initialize();
+            this.eulerRef = getContext().spawn(EulerJobProcessor.create(sourceBehavior, processorBehavior), "euler");
+            eulerRef.tell(new JobToProcess(msg.uri, msg.ctx, msg.replyTo));
+            return Behaviors.same();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Behavior<JobCommand> onJobProcessed(JobProcessed msg) {
-        return Behaviors.stopped();
+        return finishJob();
     }
 
     private Behavior<JobCommand> onCancelJob(CancelJob msg) {
-        return Behaviors.stopped();
+        return finishJob();
+    }
+
+    protected Behavior<JobCommand> finishJob() {
+        try {
+            this.hooks.close();
+            return Behaviors.stopped();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
