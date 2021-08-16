48ed6f26f7eaa272d0f2c4f4506edab871eddf1d
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2021-08-16 18:12:45 -0300

    fix for the create index initializer

--- euler-elasticsearch/src/main/java/com/github/euler/elasticsearch/CreateIndexInitializable.java
+++ euler-elasticsearch/src/main/java/com/github/euler/elasticsearch/CreateIndexInitializable.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 
 import com.github.euler.core.Initializable;
@@ -25,14 +26,22 @@ public class CreateIndexInitializable implements Initializable {
     @Override
     public void initialize() throws IOException {
         String indexName = indexConfig.getString("name");
-        CreateIndexRequest req = new CreateIndexRequest(indexName);
 
-        String jsonMappings = indexConfig.getConfig("mappings").root().render(ConfigRenderOptions.concise());
-        req.mapping(jsonMappings, XContentType.JSON);
-        String jsonSetttings = indexConfig.getConfig("settings").root().render(ConfigRenderOptions.concise());
-        req.settings(jsonSetttings, XContentType.JSON);
+        if (!exists(indexName)) {
+            CreateIndexRequest req = new CreateIndexRequest(indexName);
 
-        client.indices().create(req, RequestOptions.DEFAULT);
+            String jsonMappings = indexConfig.getConfig("mappings").root().render(ConfigRenderOptions.concise());
+            req.mapping(jsonMappings, XContentType.JSON);
+            String jsonSetttings = indexConfig.getConfig("settings").root().render(ConfigRenderOptions.concise());
+            req.settings(jsonSetttings, XContentType.JSON);
+
+            client.indices().create(req, RequestOptions.DEFAULT);
+        }
+    }
+
+    private boolean exists(String indexName) throws IOException {
+        GetIndexRequest req = new GetIndexRequest(indexName);
+        return client.indices().exists(req, RequestOptions.DEFAULT);
     }
 
 }
